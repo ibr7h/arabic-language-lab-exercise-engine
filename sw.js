9f974456613422db4ca1e83963242ba569874af4
ibr7h <d.alneami@gmail.com>
2026-09-18 08:19:06 +0300

    Refresh PWA cache for haraka and lam-alif field fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'arabic-language-lab-exercise-engine-v10-2026-09-18-ui-module';
+const CACHE_VERSION = 'arabic-language-lab-exercise-engine-v11-2026-09-18-haraka-lam-alif-field-fixes';
 const CORE = [
   './', './index.html', './manifest.webmanifest', './LICENSE.txt',
   './assets/css/tailwind.css', './assets/css/app.css',
